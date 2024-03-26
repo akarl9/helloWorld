1798ffd5b1bf0dd8fea1c803a0b94826203c364d
Abigail <akarl@terpmail.umd.edu>
2024-03-25 23:54:38 -0400

    Added the routes for favorite-course.html, and contact.html. Utilized the method GET for favorite-course.html and both: GET and POSt for contact.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,6 +19,19 @@ def about():
 def aboutcss():
     return render_template('about-css.html')
 
+@app.route('/favorite-course')
+def favorite_course():
+    print('Subject: ' + request.args.get('subject'))
+    print('Course Number: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submit=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
